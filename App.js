49070cb7aa9082a4cfd003dcbdbcacf79622ddd1
koka <muzzafarbashir116@gmail.com>
2024-02-09 12:48:49 +0530

    init project ui

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React from 'react'
+import FeedScreen from './src/screens/FeedScreen';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FeedScreen/>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'lightgray'
+      },
+})
